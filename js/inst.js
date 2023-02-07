3314eb5f2ec59386efb33a858480a435bb982457
SEIMIN123 <interkp12@gmail.com>
2023-02-07 22:15:37 +0900

    .

--- js/inst.js
+++ js/inst.js
@@ -1,5 +1,5 @@
 const root = document.getElementById('root');
 root.addEventListener('click', function(){
-  let page = "../html/ment-1.html"
+  let page = "./html/ment-1.html"
   location.href = page;
 });
